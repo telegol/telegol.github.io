e33f48b673d2e1fff01ede8ffe86ed1d5e819102
telegol <lusinmusin@gmail.com>
2025-02-09 07:42:55 +0300

    Add files via upload

---
+++ files/lika.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<video src="https://rutvzov.github.io/files/lika.mp4">
+</body>
+</html>
